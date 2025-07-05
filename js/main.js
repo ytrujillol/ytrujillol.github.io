c1ec1c39d88fd235be65e112072109fe3b794dfa
ytrujillol <ytrujillol@unal.edu.co>
2025-07-05 15:30:07 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hero = document.getElementById('hero');
+    hero.addEventListener('click', () => {
+      alert('¡Has hecho clic en la sección Hero!');
+    });
+  });
+  
